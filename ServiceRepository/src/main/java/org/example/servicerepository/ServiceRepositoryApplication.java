79effc42ec1d86cdaf0de4bc91e5c1693ea942bf
Jitu Ahir <jiturao998@gmail.com>
2024-11-12 16:16:25 +0530

    Addign ServiceReopository

---
+++ ServiceRepository/src/main/java/org/example/servicerepository/ServiceRepositoryApplication.java
@@ -0,0 +1,15 @@
+package org.example.servicerepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRepositoryApplication.class, args);
+    }
+
+}
